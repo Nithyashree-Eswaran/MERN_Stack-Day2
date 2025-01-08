77faf914dce0b208bdb258bc0063747cc744c59d
Nithyashree-Eswaran <151083472+Nithyashree-Eswaran@users.noreply.github.com>
2025-01-08 19:35:57 +0530

    Add files via upload

---
+++ Incrementor(Task)/index.js
@@ -0,0 +1,12 @@
+
+const countDisplay = document.getElementById('count');
+const incrementBtn = document.getElementById('increment-btn');
+
+
+let count = 0;
+
+
+incrementBtn.addEventListener('click', () => {
+    count++;  
+    countDisplay.textContent = count;  
+});
